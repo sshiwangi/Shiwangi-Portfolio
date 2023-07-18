7f8cd5157b2d37e9ab5c1ec11ab15359b9a692ab
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-07-18 15:23:58 +0530

    Adding footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import './footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className=" footer-items">
+      <hr className="horizontal-line"/>
+      <div className='footer-content'>Made by @sshiwangi770 | @ Copyright 2023 | All rights reserved</div>
+    </div>
   )
 }
 
