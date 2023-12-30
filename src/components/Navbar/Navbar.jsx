a272a0de6ec01295d484d8c2eefd214ebadddf3c
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-12-30 11:35:49 +0530

    fixed errors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
-import menuIcon from '../../assets/images/icon-menu-small.png'
-import './navbar.css'
-import Collapsed from '../Collapsed/Collapsed'
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import menuIcon from "../../assets/images/icon-menu-small.png";
+import "./navbar.css";
+import Collapsed from "../Collapsed/Collapsed";
 
 function Navbar() {
-  const [activeText, setActiveText] = useState('Home');
+  const [activeText, setActiveText] = useState("Home");
   const [isNavCollapsed, setIsNavCollapsed] = useState(false);
   const [isCrossShow, setIsCrossShow] = useState(false);
 
@@ -14,29 +14,78 @@ function Navbar() {
   };
 
   //handing collapse
-  function handleNavCollapse () {
+  function handleNavCollapse() {
     setIsCrossShow(!isCrossShow);
-    setIsNavCollapsed(!isNavCollapsed)
+    setIsNavCollapsed(!isNavCollapsed);
   }
-  
+
   return (
-    <header className=' header-section'>
-       <nav className="container navbar-section">
-      <Link to="/" > <div style={{color: 'white'}} onClick={()=> handleTextClick('Home')} className="brand-name"><span className='my-name'>Shiwangi</span></div></Link>
+    <header className=" header-section">
+      <nav className="container navbar-section">
+        <Link to="/">
+          {" "}
+          <div
+            style={{ color: "white" }}
+            onClick={() => handleTextClick("Home")}
+            className="brand-name"
+          >
+            <span className="my-name">Shiwangi</span>
+          </div>
+        </Link>
         <div className="menu-bar">
-          <Link to="/"  className={`buttons ${activeText === 'Home' ? 'active-text' : 'menu-content'}`} onClick={ ()=> handleTextClick('About')}>Home</Link>
-          <Link to="/about" className={`buttons ${activeText === 'About' ? 'active-text' : 'menu-content'}`} onClick={ ()=> handleTextClick('About')}>About</Link>
-          <Link to="/projects"  className={`buttons ${activeText === 'Projects' ? 'active-text' : 'menu-content'}`} onClick={()=> handleTextClick('Projects')}>Projects</Link>
-          <Link to="/experience"  className={`buttons ${activeText === 'Experience' ? 'active-text' : 'menu-content'}`} onClick={()=>handleTextClick('Experience')}>Experience</Link>
-          <Link to="/contact"  className={`buttons ${activeText === 'Contact' ? 'active-text' : 'menu-content'}`} onClick={()=> handleTextClick('Contact')}>Contact</Link>
-       </div>
-       <div onClick={handleNavCollapse} className='menu-icon hide-menu'>
-        <img height="32px" src={menuIcon} alt="menu-icon" />
-       </div>
-       </nav>
-       {isNavCollapsed && <Collapsed operation = {handleNavCollapse}/>}
+          <Link
+            to="/"
+            className={`buttons ${
+              activeText === "Home" ? "active-text" : "menu-content"
+            }`}
+            onClick={() => handleTextClick("About")}
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className={`buttons ${
+              activeText === "About" ? "active-text" : "menu-content"
+            }`}
+            onClick={() => handleTextClick("About")}
+          >
+            About
+          </Link>
+          <Link
+            to="/projects"
+            className={`buttons ${
+              activeText === "Projects" ? "active-text" : "menu-content"
+            }`}
+            onClick={() => handleTextClick("Projects")}
+          >
+            Projects
+          </Link>
+          <Link
+            to="/experience"
+            className={`buttons ${
+              activeText === "Experience" ? "active-text" : "menu-content"
+            }`}
+            onClick={() => handleTextClick("Experience")}
+          >
+            Experience
+          </Link>
+          <Link
+            to="/contact"
+            className={`buttons ${
+              activeText === "Contact" ? "active-text" : "menu-content"
+            }`}
+            onClick={() => handleTextClick("Contact")}
+          >
+            Contact
+          </Link>
+        </div>
+        <div onClick={handleNavCollapse} className="menu-icon hide-menu">
+          <img height="32px" src={menuIcon} alt="menu-icon" />
+        </div>
+      </nav>
+      {isNavCollapsed && <Collapsed operation={handleNavCollapse} />}
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,44 +1,68 @@
-import React, {useState} from 'react'
-import './Projects.css'
-import SectionHeading from '../../components/sectionHeading/SectionHeading'
-import Buttons from '../../components/button/Buttons'
-import WebDesign from '../../components/buttonslist/WebDesign'
-import WebDev from '../../components/buttonslist/WebDev'
+import React, { useState } from "react";
+import "./Projects.css";
+import SectionHeading from "../../components/sectionHeading/SectionHeading";
+import Buttons from "../../components/button/Buttons";
+import WebDesign from "../../components/buttonslist/WebDesign";
+import WebDev from "../../components/buttonslist/WebDev";
 
 function Projects() {
-  const [activeButton, setActiveButton] = useState('Web Development')
+  const [activeButton, setActiveButton] = useState("Web Development");
+  const [activeText, setActiveText] = useState(false);
+
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
-  }
-  const [activeText, setActiveText] = useState('Home');
-//handling active text state
-  const handleTextClick = (item) => {
-    setActiveText(item);
   };
-  
+
+  //handling active text state
+  const handleTextClick = () => {
+    setActiveText((prev) => !prev);
+  };
+
   const [showAllProject, setShowAllProject] = useState(false);
   //handling all project buttn click function
-    const handleAllProjectsButtonClick = () => {
-        setShowAllProject(prev => !prev);
-    }
+  const handleAllProjectsButtonClick = () => {
+    setShowAllProject((prev) => !prev);
+  };
   return (
     <div className="about-ele-section">
       <div className="container">
         <div className="heading-container">
-        <SectionHeading name="Projects" />
-        <p className={`buttons ${activeText === 'See All' ? 'active-text' : 'see-all-text'}`} onClick={()=> {handleTextClick('See All'); handleAllProjectsButtonClick();}}>See all</p>
-        </div>
-        
-        <div className="btn-container skills-btn">
-           <Buttons name="Web Development" className="skills-btn-item" onClick={handleButtonClick} isActive={activeButton === 'Web Development'}/>
-           <Buttons name="Web Design" className="skills-btn-item" onClick={handleButtonClick} isActive={activeButton === 'Web Design'} />
+          <SectionHeading name="Projects" />
+          <p
+            className={`buttons ${activeText ? "active-text" : "see-all-text"}`}
+            onClick={() => {
+              handleTextClick();
+              handleAllProjectsButtonClick();
+            }}
+          >
+            See all
+          </p>
         </div>
 
-         {activeButton === 'Web Development' && <WebDev activeButton={showAllProject}/>}
-         {activeButton === 'Web Design' && <WebDesign activeButton={showAllProject}/>}
+        <div className="btn-container skills-btn">
+          <Buttons
+            name="Web Development"
+            className="skills-btn-item"
+            onClick={handleButtonClick}
+            isActive={activeButton === "Web Development"}
+          />
+          <Buttons
+            name="Web Design"
+            className="skills-btn-item"
+            onClick={handleButtonClick}
+            isActive={activeButton === "Web Design"}
+          />
+        </div>
+
+        {activeButton === "Web Development" && (
+          <WebDev activeButton={showAllProject} />
+        )}
+        {activeButton === "Web Design" && (
+          <WebDesign activeButton={showAllProject} />
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
